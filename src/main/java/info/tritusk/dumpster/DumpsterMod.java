17f4385edf74eefa39676ded3258a0dac0e0204a
Alexander <shnupbups@gmail.com>
2019-05-14 20:24:49 +1000

    Updated to 1.14.1

--- src/main/java/info/tritusk/dumpster/DumpsterMod.java
+++ src/main/java/info/tritusk/dumpster/DumpsterMod.java
@@ -6,7 +6,7 @@ import net.minecraft.block.Material;
 import net.minecraft.block.MaterialColor;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.block.BlockItem;
+import net.minecraft.item.BlockItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -16,11 +16,11 @@ public final class DumpsterMod implements ModInitializer {
     public void onInitialize() {
         final Identifier id = new Identifier("dumpster", "dumpster");
 
-        Dumpster block = Registry.BLOCK.register(id, new Dumpster(Block.Settings.of(Material.GLASS, MaterialColor.BLACK)));
+        Dumpster block = Registry.register(Registry.BLOCK, id, new Dumpster(Block.Settings.of(Material.GLASS, MaterialColor.BLACK)));
 
         Registry.register(Registry.BLOCK_ENTITY, id, Dumpster.Logic.TYPE_TOKEN);
 
-        Registry.ITEM.register(id, new BlockItem(block, new Item.Settings().itemGroup(ItemGroup.MISC)));
+        Registry.register(Registry.ITEM, id, new BlockItem(block, new Item.Settings().itemGroup(ItemGroup.MISC)));
     }
 
 }
